8e083c07baf8a3fa0e5c3553f0cd2cb56b92f5ef
Michael Asper <michaelksasper@gmail.com>
2026-05-16 02:34:32 -0500

    feat: wire up tool registration

--- hackpi-tools/src/lib.rs
+++ hackpi-tools/src/lib.rs
@@ -3,3 +3,14 @@ pub mod edit;
 pub mod read;
 pub mod search_grep;
 pub mod write;
+
+use hackpi_core::tools::ToolRegistry;
+use std::path::PathBuf;
+
+pub fn register_all_tools(registry: &mut ToolRegistry, workspace_root: &PathBuf) {
+    registry.register(Box::new(read::ReadTool::new(workspace_root.clone())));
+    registry.register(Box::new(search_grep::SearchGrepTool::new(workspace_root.clone())));
+    registry.register(Box::new(write::WriteTool::new(workspace_root.clone())));
+    registry.register(Box::new(edit::EditTool::new(workspace_root.clone())));
+    registry.register(Box::new(bash::BashTool::new(workspace_root.clone())));
+}
